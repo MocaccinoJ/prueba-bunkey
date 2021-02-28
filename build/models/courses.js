f0ebc8b9b2c0def6d2cdbce55a5e747e23a6348c
MocaccinoJ <68201449+MocaccinoJ@users.noreply.github.com>
2021-02-27 23:28:35 -0300

    Corrections

--- build/models/courses.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
-const CoursesSchema = new Schema({
-  name: String
-});
-module.exports = mongoose.model('courses', CoursesSchema);
